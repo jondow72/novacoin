a477c93c54247ecbfbb44e1ef9b15b9f84bbe7d9
jondow72 <77569928+jondow72@users.noreply.github.com>
2026-06-24 00:29:54 +0200

    Update util.h

--- src/util.h
+++ src/util.h
@@ -145,6 +145,8 @@ inline void Sleep(int64_t n)
 extern std::map<std::string, std::string> mapArgs;
 extern std::map<std::string, std::vector<std::string> > mapMultiArgs;
 extern bool fDebug;
+extern bool fDebugMagi;
+extern bool fDebugMagiPoS;
 extern bool fDebugNet;
 extern bool fPrintToConsole;
 extern bool fPrintToDebugger;
@@ -158,6 +160,7 @@ extern bool fTestNet;
 extern bool fNoListen;
 extern bool fLogTimestamps;
 extern bool fReopenDebugLog;
+extern bool fTestNetWeightV2;
 
 int ATTR_WARN_PRINTF(1,2) OutputDebugStringF(const char* pszFormat, ...);
 
